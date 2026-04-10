0290351d63201a657d9896bca4407a9ca9c16262
LiuHongsGitHub <2875162633@qq.com>
2026-04-10 18:01:10 +0800

    17

--- exercises/17_myfile/17_myfile.c
+++ exercises/17_myfile/17_myfile.c
@@ -8,8 +8,21 @@
 void print_elf_type(uint16_t e_type) {
   const char *type_str;
   switch (e_type) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    case ET_REL:
+      type_str = "Relocatable file (.o)";
+      break;
+    case ET_EXEC:
+      type_str = "Executable file";
+      break;
+    case ET_DYN:
+      type_str = "Shared object / PIE";
+      break;
+    case ET_CORE:
+      type_str = "Core file";
+      break;
+    default:
+      type_str = "Unknown type";
+      break;
   }
   printf("ELF Type: %s (0x%x)\n", type_str, e_type);
 }
@@ -19,12 +32,22 @@ int main(int argc, char *argv[]) {
     "./17_myfile.o",
     "./17_myfile",
   };
+  int len =sizeof(filepath)/sizeof(filepath[0]);
 
   int fd;
   Elf64_Ehdr ehdr;
 
   // TODO: 在这里添加你的代码
-  // I AM NOT DONE
-  
+  for (int i = 0; i < len; i++) {
+    fd = open(filepath[i], O_RDONLY);
+    if (fd < 0) {
+      perror("open");
+      exit(1);
+    }
+    read(fd,&ehdr,sizeof(ehdr));
+    if 
+    print_elf_type(ehdr.e_type);
+    close(fd);
+  }
   return 0;
 }
